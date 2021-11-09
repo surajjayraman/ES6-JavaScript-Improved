92951a0fa356eb79905d821138a706c33c754666
surajjayraman <suraj.jayraman@gmail.com>
2021-11-09 15:46:05 -0500

    Implement spread operator.

---
+++ spread-iteration.js
@@ -0,0 +1,20 @@
+// The spread operator, gives you the ability to expand,
+//  or spread, iterable objects into multiple elements.
+
+const books = ["Don Quixote", "The Hobbit", "Alice in Wonderland", "Tale of Two Cities"];
+console.log(...books);
+
+const primes = new Set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+console.log(...primes);
+
+/*
+ * Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+ */
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+
+console.log(produce);
+
