e0021b4fc543e532904a032f1bc1116160966ac9
surajjayraman <suraj.jayraman@gmail.com>
2021-11-09 19:12:44 -0500

    Add fns for calculating sum and avg of unlimited nums using rest parameter.

--- spread-iteration.js
+++ spread-iteration.js
@@ -18,3 +18,24 @@ const produce = [...fruits, ...vegetables];
 
 console.log(produce);
 
+// Using the rest parameter: calculate sum of an unlimited amt of nums
+const mySum = function(...nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+};
+
+console.log(mySum(9, 45, 6));
+
+// create an average() function that calculates the average of an unlimited amount of numbers.
+const myAvg = function(...nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return nums.length ? total / nums.length : total;
+};
+
+console.log(myAvg(45,66,77,5, 100000, 45, 44, 6, 7));
